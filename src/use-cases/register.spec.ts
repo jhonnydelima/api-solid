03325107228c6b61733e28a08e72b1d6abf3431c
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-02 22:05:50 -0300

    test: ensure authenticate use case works properly

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -19,7 +19,6 @@ describe('Register Use Case', () => {
       email: 'johndoe@example.com',
       password: '123456',
     })
-
     expect(user.id).toEqual(expect.any(String))
   })
 
@@ -29,24 +28,20 @@ describe('Register Use Case', () => {
       email: 'johndoe@example.com',
       password: '123456',
     })
-
     const isPasswordCorrectlyHashed = await compare(
       '123456',
       user.password_hash,
     )
-
     expect(isPasswordCorrectlyHashed).toBe(true)
   })
 
   it('should not be able to register with the same email twice', async () => {
     const email = 'johndoe@example.com'
-
     await registerUseCase.execute({
       name: 'John Doe',
       email,
       password: '123456',
     })
-
     await expect(() =>
       registerUseCase.execute({
         name: 'John Doe',
