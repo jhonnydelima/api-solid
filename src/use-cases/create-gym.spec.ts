98a039c37fad22a8237f7475faf215cf1fbaf400
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-03 21:55:04 -0300

    test: ensure `create-gym` use case works properly

---
+++ src/use-cases/create-gym.spec.ts
@@ -0,0 +1,24 @@
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CreateGymUseCase } from './create-gym'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe('Create Gym Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it('should be able to create a gym', async () => {
+    const { gym } = await sut.execute({
+      title: 'Test Gym',
+      description: null,
+      phone: null,
+      latitude: -25.4705664,
+      longitude: -49.2929024,
+    })
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
