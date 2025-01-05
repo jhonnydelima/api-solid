9d6d18b8ab69c35420db46e51203a8c5ce4d40ed
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-04 23:30:37 -0300

    feat: add `fetch-user-check-ins-history` use case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -35,4 +35,11 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     }
     return checkIn
   }
+
+  async findManyByUserId(userId: string, page: number) {
+    const checkIns = this.items
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+    return checkIns
+  }
 }
---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,28 @@
+import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { CheckIn } from '@prisma/client'
+
+type FetchUserCheckInsHistoryUseCaseRequest = {
+  userId: string
+  page: number
+}
+
+type FetchUserCheckInsHistoryUseCaseResponse = {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistoryUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+    return {
+      checkIns,
+    }
+  }
+}
