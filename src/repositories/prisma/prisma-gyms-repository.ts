03dc22cb7804ffa524c3c8e1dddb9f988c95a7ed
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-08 20:24:06 -0300

    feat: finish `prisma-gyms-repository` implementation

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -1,21 +1,48 @@
-import { Prisma, Gym } from '@prisma/client'
-import { GymsRepository } from '../gyms-repository'
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
 import { prisma } from '@/lib/prisma'
 
 export class PrismaGymsRepository implements GymsRepository {
-  async create(data: Prisma.GymCreateInput): Promise<Gym> {
+  async create(data: Prisma.GymCreateInput) {
     const gym = await prisma.gym.create({
       data,
     })
     return gym
   }
 
-  async findById(id: string): Promise<Gym | null> {
+  async findById(id: string) {
     const gym = await prisma.gym.findUnique({
       where: {
         id,
       },
     })
     return gym
+  }
+
+  async findMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      orderBy: {
+        title: 'asc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+    return gyms
+  }
+
+  async findManyNearby({ latitude, longitude, page }: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * from gyms
+      WHERE (6371 * acos(cos(radians(${latitude})) * cos(radians(latitude)) * cos(radians(longitude) - radians(${longitude})) + sin(radians(${latitude})) * sin(radians(latitude)))) <= 10
+      ORDER BY title
+      LIMIT 20
+      OFFSET ${(page - 1) * 20}
+    `
+    return gyms
   }
 }
