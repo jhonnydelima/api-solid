30c0bb53230b7d7422a59ae711cdb17c4964117b
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-10 19:07:45 -0300

    feat: create `validate-check-in` controller

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,8 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { checkIn } from './check-in'
+import { validateCheckIn } from './validate-check-in'
+import { fetchUserCheckInsHistory } from './fetch-user-check-ins-history'
+import { getUserMetrics } from './get-user-metrics'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
   app.post('/gyms/:gymId/check-ins', checkIn)
+  app.get('/check-ins/history', fetchUserCheckInsHistory)
+  app.get('/check-ins/metrics', getUserMetrics)
+  app.patch('/check-ins/:checkInId/validate', validateCheckIn)
 }
