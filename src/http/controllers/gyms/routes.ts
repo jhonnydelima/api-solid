0fd4c3842af81eff2f4d761f7937ee63a367189f
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-10 18:34:23 -0300

    feat: create `search-gyms` & `fetch-nearby-gyms` controllers

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,12 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { createGym } from './create-gym'
+import { searchGyms } from './search-gyms'
+import { fetchNearbyGyms } from './fetch-nearby-gyms'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+  app.post('/gyms', createGym)
+  app.get('/gyms/search', searchGyms)
+  app.get('/gyms/nearby', fetchNearbyGyms)
 }
