0e735bf8dd5e9d563588fc1be1cde674f9f33d2c
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-11 00:58:40 -0300

    test: add `get-user-metrics controller` e2e test

---
+++ src/http/controllers/check-ins/get-user-metrics.spec.ts
@@ -0,0 +1,45 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { prisma } from '@/lib/prisma'
+
+describe('Get User Metrics e2e', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to get the user metrics', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+    const user = await prisma.user.findFirstOrThrow()
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'Test Gym',
+        latitude: -25.4705664,
+        longitude: -49.2929024,
+      },
+    })
+    await prisma.checkIn.createMany({
+      data: [
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+        {
+          gym_id: gym.id,
+          user_id: user.id,
+        },
+      ],
+    })
+    const response = await request(app.server)
+      .get('/check-ins/metrics')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+    expect(response.statusCode).toEqual(200)
+    expect(response.body.checkInsCount).toEqual(2)
+  })
+})
