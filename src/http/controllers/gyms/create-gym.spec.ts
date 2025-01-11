35095c1b5bdd2fbd9fb24d8bc3f8aff4c5d91a62
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-11 00:36:24 -0300

    test: add `create-gym controller` e2e test

--- src/http/controllers/gyms/create-gym.spec.ts
+++ src/http/controllers/gyms/create-gym.spec.ts
@@ -3,7 +3,7 @@ import { app } from '@/app'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
 
-describe('Get User Profile e2e', () => {
+describe('Create Gym e2e', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -12,17 +12,18 @@ describe('Get User Profile e2e', () => {
     await app.close()
   })
 
-  it('should be able to get user profile', async () => {
+  it('should be able to create a gym', async () => {
     const { token } = await createAndAuthenticateUser(app)
     const profileResponse = await request(app.server)
-      .get('/me')
+      .post('/gyms')
       .set('Authorization', `Bearer ${token}`)
-      .send()
-    expect(profileResponse.statusCode).toEqual(200)
-    expect(profileResponse.body.user).toEqual(
-      expect.objectContaining({
-        email: 'johndoe@example.com',
-      }),
-    )
+      .send({
+        title: 'Test Gym',
+        description: 'Test description.',
+        phone: '1122222222',
+        latitude: -25.4705664,
+        longitude: -49.2929024,
+      })
+    expect(profileResponse.statusCode).toEqual(201)
   })
 })
