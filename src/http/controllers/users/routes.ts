dbe177d94a463a8bf7229c7bee13aa04d05e4eba
Jhonny Lima <lima.jhonny95@gmail.com>
2025-01-11 23:11:28 -0300

    feat: add `refresh-token` route

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,11 +3,13 @@ import { register } from './register'
 import { authenticate } from './authenticate'
 import { getUserProfile } from './get-user-profile'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { refreshToken } from './refresh-token'
 
 export async function usersRoutes(app: FastifyInstance) {
   // PUBLIC ROUTES
   app.post('/users', register)
   app.post('/sessions', authenticate)
+  app.patch('/token/refresh', refreshToken)
 
   // PRIVATE ROUTES
   app.get('/me', { onRequest: [verifyJWT] }, getUserProfile)
